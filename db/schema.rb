bb26b81bd6f638446da0b850c48b1344ac03d735
nittamatama <blackmnshin@gmail.com>
2021-12-27 21:57:39 +0900

    roleカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_121151) do
+ActiveRecord::Schema.define(version: 2021_12_27_125407) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role", default: 0, null: false
   end
 
 end
